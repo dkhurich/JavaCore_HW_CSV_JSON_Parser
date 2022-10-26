f5ba458b3c050ea9f9b5e82767341a5dc5b82a0e
Denis Huric <dhuric231@gmail.com>
2022-10-26 15:16:16 +0200

    add second task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,11 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -17,8 +21,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static final String FILENAME = "data.csv";
+    public static final String FILE_NAME_CSV = "data.csv";
+    public static final String FILE_NAME_XML = "data.xml";
     public static final String[] COLUMN_MAPPING = {"id", "firstName", "lastName", "country", "age"};
+    public static int coutFiles = 1;
 
     public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
         List<Employee> employeeList = new ArrayList<>();
@@ -46,7 +52,7 @@ public class Main {
     }
 
     public static void writeString(String json) {
-        try (FileWriter file = new FileWriter("data.json")) {
+        try (FileWriter file = new FileWriter("data" + coutFiles + ".json")) {
             file.write(json);
             file.flush();
         } catch (IOException e) {
@@ -54,8 +60,44 @@ public class Main {
         }
     }
 
+    public static List<Employee> parseXML(String file) {
+        List<Employee> employeeList = new ArrayList<Employee>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new File(FILE_NAME_XML));
+            Node root = doc.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (Node.ELEMENT_NODE == node.getNodeType()) {
+                    Element element = (Element) node;
+                    NodeList node1 = element.getElementsByTagName("id");
+                    String id = node1.item(0).getTextContent();
+                    NodeList node2 = element.getElementsByTagName("firstName");
+                    String firstName = node2.item(0).getTextContent();
+                    NodeList node3 = element.getElementsByTagName("lastName");
+                    String lastName = node3.item(0).getTextContent();
+                    NodeList node4 = element.getElementsByTagName("country");
+                    String country = node4.item(0).getTextContent();
+                    NodeList node5 = element.getElementsByTagName("age");
+                    String age = node5.item(0).getTextContent();
+                    employeeList.add(new Employee(Long.parseLong(id), firstName, lastName, country, Integer.parseInt(age)));
+                }
+            }
+
+        } catch (Exception ex) {
+            System.out.println(Arrays.toString(ex.getStackTrace()));
+        }
+        return employeeList;
+    }
+
+
     public static void main(String[] args) {
-        List<Employee> list = parseCSV(COLUMN_MAPPING, FILENAME);
-        listToJson(list);
+        List<Employee> listCSV = parseCSV(COLUMN_MAPPING, FILE_NAME_CSV);
+        listToJson(listCSV);
+        coutFiles++;
+        List<Employee> listXML = parseXML(FILE_NAME_XML);
+        listToJson(listXML);
     }
 }
